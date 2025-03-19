9536ec08cd4e1d56fd97d2c12d7482b6168ed683
nvkt <nevermindktab@gmail.com>
2025-03-19 13:25:33 +0200

    four

--- Repos/OrderRepo.cs
+++ Repos/OrderRepo.cs
@@ -4,11 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Storecd.Repos
 {
-    class OrderRepo : Repository<Order>
+    public class OrderRepo : Repository<Order>
     {
         public OrderRepo(MyDbContext context) : base(context) { }
+
+        public List<Order> GetAllOrdersWithUsers()
+        {
+            using var context = new MyDbContext();
+
+            var orders = context.Orders?
+                .Include(o => o.IdUser)
+                .Include(o => o.IdProduct)
+                .ToList();
+
+            return orders;
+        }
     }
 }
--- Repos/ProductRepo.cs
+++ Repos/ProductRepo.cs
@@ -4,11 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Storecd.Repos
 {
-    class ProductRepo : Repository<Product>
+    public class ProductRepo : Repository<Product>
     {
         public ProductRepo(MyDbContext context) : base(context) { }
+
+        public List<Product> GetAllProductsWithCtegories()
+        {
+            using var context = new MyDbContext();
+
+            var products = context.Products?
+                .Include(p => p.IdCategory).ToList();
+
+            return products;
+        }
     }
 }
